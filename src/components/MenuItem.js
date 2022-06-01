555cda88f741fc17200353f6a36bd247592c9930
Leandro Oliveira Pereira <leandrobhte1@hotmail.com>
2022-06-01 17:08:12 -0300

    ajustes search empresas

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,11 +1,39 @@
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+import { useResultSearchContext } from '../hooks/useResultSearchContext'
 
 const MenuItem = (props) => {
+  const navigate = useNavigate()
+  const { result, dispatchResult } = useResultSearchContext();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    axios.get(`http://localhost:8080/api/empresas/search?searchTerm=`, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        }
+    })
+    .then(resp => {
+        let dados = resp.data;
+        dispatchResult({type: "SEARCH_RESULT", payload: dados})
+        
+    });
+    navigate(`/search?searchTerm=`);
+  }
+
   return (
     <li>
-        <Link to={props.path}>
-            <span>{props.label}</span>
-        </Link>
+      {props.path == 'barbearias' && (
+          <Link to={props.path} onClick={handleSearch}>
+              <span>{props.label}</span>
+          </Link>
+      )}
+      {props.path != 'barbearias' && (
+          <Link to={props.path}>
+              <span>{props.label}</span>
+          </Link>
+      )}
     </li>
   )
 }
