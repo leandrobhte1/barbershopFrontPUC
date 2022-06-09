e5e6207c92d53603fbe73e78b437dc298ddecb1a
Leandro Oliveira Pereira <leandrobhte1@hotmail.com>
2022-06-09 16:20:48 -0300

    deploy connecting to back

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,10 +2,23 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
 import { useResultSearchContext } from '../hooks/useResultSearchContext'
+import { ToastContainer, toast } from 'react-toastify';
 
 const MenuItem = (props) => {
   const navigate = useNavigate()
   const { result, dispatchResult } = useResultSearchContext();
+
+  const handleNoPage = (e) => {
+    toast.info('Tela não foi criada! Não era requisito criar todas as telas do sistema!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+  });
+  }
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -30,9 +43,9 @@ const MenuItem = (props) => {
           </Link>
       )}
       {props.path != 'barbearias' && (
-          <Link to={props.path}>
-              <span>{props.label}</span>
-          </Link>
+          // <Link to={props.path}>
+              <span onClick={handleNoPage}>{props.label}</span>
+          // </Link>
       )}
     </li>
   )
